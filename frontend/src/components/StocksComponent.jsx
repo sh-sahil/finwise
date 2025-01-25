0eb90dc3c4aaa7a3660bd88915bcc50e1f7eaf16
Presha Karan <karanpresha@gmail.com>
2025-01-26 03:04:00 +0530

    added stock

--- frontend/src/components/StocksComponent.jsx
+++ frontend/src/components/StocksComponent.jsx
@@ -1,22 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const StocksComponent = () => {
+  const [showRealTime, setShowRealTime] = useState(false);
+  const [showHistorical, setShowHistorical] = useState(false);
+
   return (
     <div className="w-full h-screen">
-        <button>
-            Real-Time Stocks
-        </button>
-        <button>
-            Historical Stocks
-        </button>
-      <iframe
-        src="http://localhost:8503"
-        title="Stocks Analysis"
-        width="100%"
-        height="100%"
-        style={{ border: 'none' }}
-        allowFullScreen
-      />
+      <button onClick={() => {
+          setShowRealTime(!showRealTime);
+          setShowHistorical(false);
+        }} className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mx-10 rounded my-5'>
+        Real-Time Stocks
+      </button>
+      <button  onClick={() => {
+          setShowHistorical(!showHistorical);
+          setShowRealTime(false);
+        }} className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mx-10 rounded my-5'>
+        Historical Stocks
+      </button>
+
+      {showRealTime && (
+        <iframe
+          src="http://localhost:8503"
+          title="Stocks Real-Time Analysis"
+          width="100%"
+          height="100%"
+          style={{ border: 'none' }}
+          allowFullScreen
+        />
+      )}
+
+      {showHistorical && (
+        <iframe
+          src="http://localhost:8501"
+          title="Stocks Historical Analysis"
+          width="100%"
+          height="100%"
+          style={{ border: 'none' }}
+          allowFullScreen
+        />
+      )}
     </div>
   );
 };
